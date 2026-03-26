1221ce1d3c888e1576794472073f26900ff421f1
Sharad <ksfbd100@gmail.com>
2026-03-26 23:00:46 +0530

    functions

---
+++ src/Loops/methods.java
@@ -0,0 +1,23 @@
+package Loops;
+
+import java.util.Scanner;
+
+public class methods {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.print("How many times to print printText?: ");
+//        int a = input.nextInt();
+//        for (int i=0; i<a; i++){
+//            printText();
+//        }
+
+    }
+
+
+    //    EXERCISE 1 - Create a method called printText which prints the phrase "In a hole in the ground there lived a method" and a newline.
+//    static void printText(){
+//        System.out.println("In a hole in the ground there lived a method\n");
+//}
+
+
+}
