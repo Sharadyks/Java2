a48420a62494f4b9bf450d809ce4aebbad314981
Sharad <ksfbd100@gmail.com>
2026-08-26 20:34:03 +0530

    Learned about ArrayList

--- src/Collections/ArrayList/Example3.java
+++ src/Collections/ArrayList/Example3.java
@@ -1,8 +1,21 @@
 package Collections.ArrayList;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
+class StringComparator implements Comparator<String>{
+    @Override
+    public int compare(String o1, String o2) {
+        return o1.length()-o2.length();
+    }
+}
+class MyComparator implements Comparator<Integer>{
+    @Override
+    public int compare(Integer o1, Integer o2) {
+        return o2-o1;
+    }
+}
 public class Example3 {
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
@@ -11,7 +24,18 @@ public class Example3 {
         list.add(2);
         list.add(6);
 
-        list.sort(null);
+        list.sort(new MyComparator());
         System.out.println(list);
+
+
+        List<String> list1 = new ArrayList<>();
+        list1.add("Sharad");
+        list1.add("Sharma");
+        list1.add("Hello");
+        list1.add("Me");
+        list1.add("Discrimination");
+
+        list1.sort(new StringComparator());
+        System.out.println(list1);
     }
 }
